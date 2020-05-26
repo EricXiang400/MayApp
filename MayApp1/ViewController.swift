915b1938ce4c2a057f224f7d01041eac2cddfe3e
Calvin Du <abcalvind@gmail.com>
2020-05-26 13:05:24 -0700

    Initial Commit

---
+++ MayApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MayApp1
+//
+//  Created by Calvin Du on 5/26/20.
+//  Copyright © 2020 Calvin Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
